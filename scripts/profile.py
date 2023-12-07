b5eab78856a11c6e158d3e0a93c7ff41eb5e0d46
Sarthak Katyal <sarthakkatyal@wirelessprv-10-195-33-61.near.illinois.edu>
2023-12-06 18:52:33 -0600

    scripts

--- scripts/profile.py
+++ scripts/profile.py
@@ -1,9 +1,9 @@
 import pandas as pd
 from ydata_profiling import ProfileReport
 
-# Setting up the Datafram
+# Setting up the Dataframe
 column_names = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'class']
-df = pd.read_csv('.data/iris/iris.data', header=None, names=column_names)
+df = pd.read_csv('data/iris/iris.data', header=None, names=column_names)
 
 # Generating the Report
 profile_report = ProfileReport(df,title= 'Profiling Report')
